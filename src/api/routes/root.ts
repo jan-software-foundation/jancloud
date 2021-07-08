f2832a602aa1c9c7652cf8e84eeaeb96bc64d94a
skybird23333 <2603003199a@gmail.com>
2021-07-08 21:47:54 +0800

    Refactor: make vue thing work

--- src/api/routes/root.ts
+++ src/api/routes/root.ts
@@ -3,7 +3,7 @@ import express from 'express';
 export async function register(app: express.Application) {
     app.get('/api', (req: express.Request, res: express.Response) => {
         let json = {
-            "version": require('../../../package.json').version
+            "version": require('../../../../jancloud-old/package.json').version
         }
         res.send(json);
     });
--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,19 @@
 require('dotenv').config();
 
-import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 import history from 'connect-history-api-fallback';
 
 const app = express();
 
 (async () => {
-    await require('./api/index').init(app);
+    //await require('./api/index').init(app);
+    
+    app.get('/', (req, res) => res.sendFile('/public/index.html', {root: path.join(process.cwd())}))
+
+    app.use('/', express.static('public'));
     
     app.use(logger('dev'));
     app.use(express.json());
@@ -18,24 +21,6 @@ const app = express();
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(history());
-    app.set('views', './views')
-    app.set('view engine', 'ejs')
-    
-    app.get('/', (req, res, next) => {
-        return res.render('index', {ip: req.headers['x-forwarded-for'] || req.socket.remoteAddress });
-    });
-    
-    app.use(function(req, res, next) {
-        next(createError(404));
-    });
-    
-    app.use(function(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
-        
-        res.status(err.status || 500);
-        res.send('error');
-    });
     
     
     app.listen(8080, () => console.log('Listening on http://localhost:8080'));
