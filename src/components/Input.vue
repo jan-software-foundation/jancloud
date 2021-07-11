18aa337465e682c6c7ad129f4976a571ed6c4d39
skybird23333 <2603003199a@gmail.com>
2021-07-11 21:23:44 +0800

    Feat components: add Input

---
+++ src/components/Input.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="input">
+    <label class="inputlabel" v-if="label">{{name}}</label>
+    <input type="text" :placeholder="placeholder || `Enter ${name}`" :required="required" :id="id || name.toLowerCase()">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Input",
+  props: {
+    name: String,
+    id: String,
+    placeholder: String,
+    label: Boolean,
+    required: Boolean
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+
+.inputlabel{
+  text-align: left;
+  margin-left: 10px;
+}
+
+input {
+  width: 100%;
+  padding: 12px 20px;
+  display: inline-block;
+  border: 1px solid var(--backgroundDark);
+  background-color: var(--backgroundLight);
+  box-sizing: border-box;
+  transition-duration: 0.1s;
+}
+input:hover{
+  background-color: var(--foregroundDark);
+}
+
+input:focus{
+  background-image: linear-gradient(var(--foregroundDark),var(--foreground));
+  color: var(--background);
+  outline: none;
+}
+</style>
