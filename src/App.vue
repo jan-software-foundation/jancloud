3f093f0d60b6acb8ade870b23f652c5d81d10a3d
skybird23333 <2603003199a@gmail.com>
2021-07-11 21:26:46 +0800

    Feat App: Cool background thing

--- src/App.vue
+++ src/App.vue
@@ -21,12 +21,55 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+}
+@-webkit-keyframes background {
+  0%{background-position:50% 0%}
+  50%{background-position:51% 100%}
+  100%{background-position:50% 0%}
+}
+@-moz-keyframes background {
+  0%{background-position:50% 0%}
+  50%{background-position:51% 100%}
+  100%{background-position:50% 0%}
+}
+@-o-keyframes background {
+  0%{background-position:50% 0%}
+  50%{background-position:51% 100%}
+  100%{background-position:50% 0%}
+}
+@keyframes background {
+  0%{background-position:50% 0%}
+  50%{background-position:51% 100%}
+  100%{background-position:50% 0%}
+}
+
+.background {
+  background: linear-gradient(180deg, var(--backgroundDark) 70% , var(--foregroundDark));
+  background-size: 120% 120%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  z-index: -100;
+  -webkit-animation: background 15s ease infinite;
+  -moz-animation: background 15s ease infinite;
+  -o-animation: background 15s ease infinite;
+  animation: background 15s ease infinite;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
 }
-</style>
+* {
+  color: var(--font)
+}
+  </style>
