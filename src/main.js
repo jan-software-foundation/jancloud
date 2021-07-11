dd3d77f903d917a186c9ecf45b06f4659ede67c6
skybird23333 <2603003199a@gmail.com>
2021-07-11 21:23:13 +0800

    Feat main.js: Added vue router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Login from './Login.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
 new Vue({
+  router: new VueRouter({
+    mode: 'history',
+    routes: [
+      {
+        path: '/login',
+        component: Login
+      }
+    ]
+  }),
   render: h => h(App),
 }).$mount('#app')
