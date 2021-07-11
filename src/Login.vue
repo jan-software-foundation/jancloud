3c4d2d788719fa847f37a8e856bb7d747347fc4e
skybird23333 <2603003199a@gmail.com>
2021-07-11 21:25:19 +0800

    Feat Login: Page now uses css vars correctly

---
+++ src/Login.vue
@@ -0,0 +1,42 @@
+    <div class="container" style="background-image: linear-gradient(var(--foreground),var(--foregroundDark))">
+        <h1 style="padding: 10px;color:var(--background)">Log in to JanCloud</h1>
+    </div>
+<style scoped>
+
+.login {
+  width: 469px;
+  margin: auto;
+  background-color: var(--backgroundLight);
+}
+
+button {
+  background-color: var(--backgroundLight);
+  color: var(--font);
+  padding: 14px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  transition-duration: 100ms;
+}
+
+button:hover {
+  background-color: var(--foregroundDark);
+  color: var(--background);
+}
+
+.switchbtn {
+  background-color: var(--backgroundLight);
+  color: var(--font)
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 300px) {
+  span.psw {
+    display: block;
+    float: none;
+  }
+  .switchbtn {
+    width: 100%;
+  }
+} 
+</style>
