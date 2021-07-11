d0890d72bb2e2604d477a9ff6b65f3c11ad057e5
skybird23333 <2603003199a@gmail.com>
2021-07-11 21:23:55 +0800

    Feat Components: Add Nagivation

---
+++ src/components/Navigation.vue
@@ -0,0 +1,52 @@
+<template>
+  <ul class="sidenav">
+    <li><router-link to="/home">Home</router-link></li>
+    <div style="float:right">
+      <li><router-link to="/login">Login</router-link></li>
+    </div>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Navigation"
+}
+</script>
+
+<style scoped>
+ul.sidenav {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  background-color: var(--background);
+  overflow: auto;
+  width: 100%;
+  height: auto;
+  position: relative;
+}
+
+ul.sidenav li a, router-link {
+  color: var(--font);
+  display: block;
+  text-decoration: none;
+  float: left;
+  padding: 15px;
+}
+
+.router-link-active {
+  background-color: var(--foregroundDark);
+  color: white;
+}
+
+ul.sidenav li a:hover:not(.active) {
+  background-color: var(--foreground);
+  color: white;
+}
+
+@media screen and (max-width: 400px) {
+  ul.sidenav li a {
+    text-align: center;
+    float: none;
+  }
+}
+</style>
