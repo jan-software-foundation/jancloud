dee14f9c60688314e213bb85deb07cc786beab8e
skybird23333 <2603003199a@gmail.com>
2021-07-11 21:25:45 +0800

    Refactor Login: Page now uses Input Component

--- src/Login.vue
+++ src/Login.vue
@@ -1,6 +1,32 @@
+<template>
+  <div class="login">
     <div class="container" style="background-image: linear-gradient(var(--foreground),var(--foregroundDark))">
         <h1 style="padding: 10px;color:var(--background)">Log in to JanCloud</h1>
     </div>
+
+    <div class="container">
+      <Input name="Username" label></Input>
+      <Input name="Password" label></Input>
+
+      <button type="submit">Login</button>
+      <button type="button" class="switchbtn">Sign up</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Input from './components/Input.vue'
+//TODO: Button Component
+export default {
+    components: {
+        Input
+  },
+  name: 'Login',
+  methods: {
+  }
+}
+</script>
+
 <style scoped>
 
 .login {
